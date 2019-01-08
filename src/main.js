6b7335e938ae47619e84cba3ce1bcc60fc0272d0
Max Campbell <maxc@maxc.in>
2019-01-08 21:09:46 +0100

    fixed relative urls

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // Register the service worker
 import M from 'materialize-css';
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+	navigator.serviceWorker.register('service-worker.js').then(function(registration) {
     // Registration was successful
     console.log('ServiceWorker registration successful with scope: ', registration.scope);
 }).catch(function(err) {
